26ad5b0b459d45fae2b4ec4a39f6bd4418dd1788
Manuel Andreo Garcia <manuel@magware.dev>
2024-02-10 23:07:44 +0100

    main view with TopAppBar

--- app/src/main/java/com/example/course/Main.kt
+++ app/src/main/java/com/example/course/Main.kt
@@ -5,13 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,7 +28,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.course.ui.theme.AppTheme
 import com.example.course.unit2.DiceApp
 import com.example.course.unit2.GalleryApp
@@ -60,46 +67,48 @@ class Main : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     private fun View() {
         var currentApp: App? by remember { mutableStateOf(null) }
-        if (currentApp == null) {
-            Home { currentApp = it }
-        } else {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Button(
-                    onClick = { currentApp = null },
-                    modifier = Modifier.padding(8.dp)
+        Column {
+            TopAppBar(title = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    Text("return to app selector")
+                    IconButton(onClick = { currentApp = null }) {
+                        Icon(Icons.Filled.Home, contentDescription = null)
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Android Course Apps")
+                    }
                 }
+            })
+            if (currentApp == null) {
+                Home { currentApp = it }
+            } else {
                 Surface(Modifier.fillMaxSize()) {
                     currentApp!!.Composable()
                 }
-
             }
         }
     }
 
     @Composable
     private fun Home(navigate: (App) -> Unit) {
-        Column(
+        LazyColumn(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-            LazyColumn {
-                items(App.entries.toList()) {
-                    Button(onClick = { navigate(it) }) {
-                        Text(text = it.title)
-                    }
+            items(App.entries.toList()) {
+                Button(onClick = { navigate(it) }) {
+                    Text(text = it.title, style = MaterialTheme.typography.bodyLarge)
                 }
             }
         }
     }
-
 }
